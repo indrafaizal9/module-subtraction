da255a93a85f66397c77734a0f844d6478ebf108
Indra Faizal Amri <faizalindra9@gmail.com>
2024-04-22 13:57:14 +0700

    Add subtraction API route

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Indra\ModuleSubtraction\Controllers\SubtractionController;
 
+Route::mergeWithLastGroup(['prefix' => 'subtraction', 'middleware' => 'api'], function () {
+    Route::get('/subtract', [SubtractionController::class, 'subtract']);
+});
 
-Route::get('/subtract', [SubtractionController::class, 'subtract']);
+Route::get('/check', function () {
+    return 'Module Subtraction is working!';
+});
+
+Route::get('/api/check', function () {
+    return 'Module Subtraction is working!';
+});
