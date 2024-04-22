b31447dc7df4ea369dc685acf5010e20f44eeeac
Indra Faizal Amri <faizalindra9@gmail.com>
2024-04-22 11:31:36 +0700

    Fix Package Discovery

--- src/Providers/ModuleSubtractionServiceProvider.php
+++ src/Providers/ModuleSubtractionServiceProvider.php
@@ -9,6 +9,5 @@ final class ModuleSubtractionServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        // $this->loadViewsFrom(__DIR__ . '/../views', 'module-subtraction');
     }
 }
