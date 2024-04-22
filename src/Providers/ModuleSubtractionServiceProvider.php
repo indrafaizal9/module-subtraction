f8d2c81615a22255f2d2c2e76bd13e1a5037d38a
Indra Faizal Amri <faizalindra9@gmail.com>
2024-04-22 11:33:06 +0700

    FIx load routes

--- src/Providers/ModuleSubtractionServiceProvider.php
+++ src/Providers/ModuleSubtractionServiceProvider.php
@@ -8,6 +8,6 @@ final class ModuleSubtractionServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
     }
 }
